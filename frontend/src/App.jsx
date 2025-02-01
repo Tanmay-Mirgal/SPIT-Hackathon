bccc48e5c62c802a97c7017d6668c7bd8ae377c3
Tanmay-Mirgal <tanmaymirgal26@gmail.com>
2025-02-01 15:21:30 +0530

    Doine

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,9 @@ import Home from '@/pages/Home/Home'
 import Login from '@/pages/Login/Login'
 import Signup from '@/pages/Signup/Signup'
 import HealthDashboard from '@/pages/Dashboard/Dashboard'
-import Schedule from '@/pages/Meeting/page'
+
 import Navbar from '@/pages/Home/components/Navbar'
-import MeetingPage from './pages/Meeting/Meeting'
+
 import Footer from './pages/Home/components/Footer'
 // import MeetingPage from '@/pages/Meeting/Meeting'
 import ChatInterface from './pages/Chat/ChatPage'
@@ -24,8 +24,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<HealthDashboard />} />
-        <Route path="/schedule" element={<Schedule />} />
-        <Route path="/conference" element={<MeetingPage/>} />
+ 
         <Route path='/chat' element={<ChatInterface/>}/>
         <Route path="/doctor-list" element={<Doctors/>} />
         {/* <Route path="/about" element={<About />} /> */}
